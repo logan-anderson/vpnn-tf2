32537ed659a5a55b31967c2b598ba4b2ce7b250c
Logan Anderson <landerson2@upei.ca>
2020-10-01 10:55:01 -0300

    added testing file

---
+++ testing.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def customPremute(dim, max=5):
+    # take a number and put it in the top 5 available spots
+    # prem = [-1 for i in range(dim)]
+    perm = []
+
+    # numbers from 0 to dim-1
+    numbers = [i for i in range(dim)]
+
+    currentMax = np.array(numbers[:max])
+
+    for i in range(dim):
+
+        #  shuffle the current top
+        asdf = np.array(currentMax)
+        np.random.shuffle(asdf)
+        currentMax = asdf.tolist()
+
+        perm.append(currentMax.pop(0))
+
+        if(i+max < dim):
+            currentMax.append(numbers[i+max])
+
+    return perm
+
+
+print(customPremute(10, max=2))
