231dd96cfe9768e8aeddc17cd0280bcc85fedcec
logan-anderson <landerson2@upei.ca>
2020-12-15 15:53:55 -0400

    small test

--- run_batch_jobs.py
+++ run_batch_jobs.py
@@ -15,15 +15,18 @@ job_directory = "%s/.jobs" % os.getcwd()
 
 # Make top level directories
 mkdir_p(job_directory)
+mkdir_p(job_directory + '/img')
+
 # mkdir_p(data_dir)
 
 max_ = 3
-min = 1
-jobs = [
-    {"layers": i, "rotations": j, "title": f"tf-vpnn-spatical-mixedlayers-{i}-rotations-{j}"} for i in range(1, max_, 2) for j in range(1, max_, 2)
-]
+min_ = 1
+jobs = []
+for i in range(1, max_, 2):
+    for j in range(1, max_, 2):
+        jobs.append({"layers": i, "rotations": j,
+                     "title": f"tf-vpnn-spatical-mixedlayers-{i}-rotations-{j}"})
 
-mkdir_p(os.path.join(j))
 
 for job in jobs:
 
@@ -31,8 +34,8 @@ for job in jobs:
 
     layers = job['layers']
     rotations = job['rotations']
-
-    with open(job_file) as fh:
+    mode = 'a' if os.path.exists(job_file) else 'w'
+    with open(job_file, mode) as fh:
         fh.writelines(f"""
 #SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M        # memory per node
