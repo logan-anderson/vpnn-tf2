76f05ac24284c64ba3b53f8218879da795948b05
logan-anderson <landerson2@upei.ca>
2020-12-16 09:06:22 -0400

    update path

--- run_batch_jobs.py
+++ run_batch_jobs.py
@@ -1,7 +1,5 @@
 import os
 
-import json
-
 
 def mkdir_p(dir):
     '''make a directory (dir) if it doesn't exist'''
@@ -10,6 +8,7 @@ def mkdir_p(dir):
 
 
 job_directory = "%s/.jobs" % os.getcwd()
+file = os.path.join(os.getcwd(), 'run_demo_hyper.py')
 # scratch = os.environ['SCRATCH']
 # data_dir = os.path.join(scratch, '/spatical')
 
@@ -41,13 +40,13 @@ for job in jobs:
         fh.writelines(f"""# SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 # SBATCH --mem=32000M        # memory per node
 # SBATCH --time=0-20:00      # time (DD-HH:MM)
-# SBATCH --output=hyper-fixed-mnist-layers-{layers}-rotations-{rotations}_dropout-.2%N-%j.out  # %N for node name, %j for jobID
+# SBATCH --output=hyper-batch-fixed-mnist-layers-{layers}-rotations-{rotations}_dropout-.2%N-%j.out  # %N for node name, %j for jobID
 # SBATCH --gres=gpu:v100:1
 
 module load cuda cudnn
 source ../tensorflow/bin/activate
 
-python run_demo_hyper.py --layers {layers} --rotations {rotations} --use_dropout True --total_runs 28 --epochs 300 --permutation_arrangement 4
+python {file} --layers {layers} --rotations {rotations} --use_dropout True --total_runs 28 --epochs 300 --permutation_arrangement 4
         """)
 
     os.system("sbatch %s" % job_file)
