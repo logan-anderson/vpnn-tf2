4ff46c5e88c06e6e98bd40eca88bb3315bb1725e
logan-anderson <landerson2@upei.ca>
2021-01-18 09:37:52 -0400

    updated time

--- mnsit_jobs/mnsit-random.sh
+++ mnsit_jobs/mnsit-random.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M        # memory per node
 #SBATCH --time=06:00      # time (DD-HH:MM)
